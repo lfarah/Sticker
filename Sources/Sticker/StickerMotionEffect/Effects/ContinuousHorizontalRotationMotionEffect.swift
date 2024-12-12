565bb52c14646c616fd9d7804a3d9a884009f756
Lucas Farah <lucas.farah@me.com>
2024-12-12 17:16:04 -0300

    fix for the fix

--- Sources/Sticker/StickerMotionEffect/Effects/ContinuousHorizontalRotationMotionEffect.swift
+++ Sources/Sticker/StickerMotionEffect/Effects/ContinuousHorizontalRotationMotionEffect.swift
@@ -31,7 +31,7 @@ public struct ContinuousHorizontalRotationMotionEffect: StickerMotionEffect {
                         // Update shader with new rotation values
                         shaderUpdater.update(
                             with: .init(
-                                x: rotationAngle * size.height / 2,
+                                x: rotationAngle * size.width / 2,
                                 y: 0 // Static Y-axis as this is only horizontal rotation
                             )
                         )
